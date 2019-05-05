6e2d8abfb3a3796a82f82ad876fcd1d10a58c931
VanDarkClaus <1028541499@qq.com>
2019-05-05 11:54:20 +0800

    feat:头部添加样式

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 module.exports = {
     //配置代理
     devServer: {
+      open: 'true',
       proxy: {
         '/todos': {
-          target: 'http://jsonplaceholder.typicode.com',
+          target: 'http://www.xiongmaoyouxuan.com',
           ws: true,
           changeOrigin: true
         }
